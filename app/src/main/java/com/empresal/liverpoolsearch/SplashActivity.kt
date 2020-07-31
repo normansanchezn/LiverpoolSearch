643e026dbac39cf328f0bc85acffb874b4baa940
Norman SN <normansanchezn@gmail.com>
2020-07-31 16:53:58 -0500

    **Feature: Splash screen

---
+++ app/src/main/java/com/empresal/liverpoolsearch/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.empresal.liverpoolsearch
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT:Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, SearchActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
